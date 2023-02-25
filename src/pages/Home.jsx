a74653a28332c0f83911a9b7aa3895bcf544fba4
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-02-25 22:17:42 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,34 +3,44 @@ import React, {useState, useEffect} from 'react'
 const Home = () => {
   let [news, setNews] = useState([]);
 
-  useEffect (() =>{
+  useEffect(() => {
     getNews();
   }, []);
 
-  useEffect (() =>{
-    console.log(news)
+  useEffect(() => {
+    console.log(news);
   }, [news]);
 
-  const BASE_URL = 'https://admin.ikar-thinktank.org/api';
-   const getNews = async () => {
-    fetch(`${BASE_URL}/news`)
-      .then(response => response.json())
-      .then(res => {
+  const BASE_URL = "https://admin.ikar-thinktank.org/api";
+  const getNews = async () => {
+    await fetch(`${BASE_URL}/news`)
+      .then((response) => response.json())
+      .then((res) => {
         const CLEARED_DATA = res.data.map((item) => {
-          return{
+          return {
             id: item.id,
-            ...item.atributes 
-          }
-        });     
+            ...item.attributes,
+          };
+        });
         setNews(CLEARED_DATA);
       });
-   }
-  return (
-    <div>Home</div>
-   
-  )
-}
+  };
 
-export default Home
+  return (
+    <>
+      <div>Home</div>
+      {news.map((item) => {
+        return (
+          <div key={item.id}>
+            <p>{item.title}</p>
+            <p>{item.text}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Home;
 
 
